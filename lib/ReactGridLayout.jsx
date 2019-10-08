9d528363b32815071d2641f27e5d8e826b1a36c7
Alexander Zayka <48759604+zaykaalexander@users.noreply.github.com>
2019-10-08 22:30:14 +0700

    Fix freezing on drag ie11
    https://github.com/STRML/react-grid-layout/issues/760
    
    1) Add shouldComponentUpdate for ReactGridLayout
    2) Add shouldComponentUpdate for GridItem
    3) Fix when items are dragging not from own position, but from zero top left

--- lib/ReactGridLayout.jsx
+++ lib/ReactGridLayout.jsx
@@ -14,7 +14,8 @@ import {
   synchronizeLayoutWithChildren,
   validateLayout,
   getAllCollisions,
-  noop
+  noop,
+  pick
 } from "./utils";
 import GridItem from "./GridItem";
 import type {
@@ -105,6 +106,33 @@ try {
 } catch (e) {
   /* Ignore */
 }
+
+/**
+ * Array of keys for sCU
+ */
+const pickPropsKeys = [
+  "className",
+  "style",
+  "width",
+  "autoSize",
+  "cols",
+  "draggableCancel",
+  "draggableHandle",
+  "verticalCompact",
+  "compactType",
+  "layout",
+  "margin",
+  "containerPadding",
+  "rowHeight",
+  "maxRows",
+  "isDraggable",
+  "isResizable",
+  "isDroppable",
+  "preventCollision",
+  "useCSSTransforms",
+  "transformScale",
+  "droppingItem"
+];
 
 /**
  * A reactive, fluid grid layout with draggable, resizable components.
@@ -358,6 +386,15 @@ export default class ReactGridLayout extends React.Component<Props, State> {
     }
 
     return null;
+  }
+
+  shouldComponentUpdate(nextProps: Props, nextState: State) {
+    return (
+      !isEqual(
+        pick(pickPropsKeys, this.props),
+        pick(pickPropsKeys, nextProps)
+      ) || !isEqual(this.state.activeDrag, nextState.activeDrag)
+    );
   }
 
   componentDidUpdate(prevProps: Props, prevState: State) {
